6bff885ef920c1bca5f11a2f7c6b99d21b159198
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-19 09:37:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Topic_4___Input_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            int age;
+            double money;
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you, " + name + ", How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("So you're " + age + " eh? Thats not old at all! How much do you make " + name + "?");
+            money = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(); + money + "! I hope thats per hour and not per year");
+            
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
