7f63aac6276c09e6fe390355ac8304bbca7687ff
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-20 09:38:04 -0400

    FInished question 1 2 and 3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
+            //Part1();
 
+            Part2();
+
+            //Part3();
+        }
+
+        public static void Part2()
+        {
+            string name, login;
+            int ID, grade;
+           
+           
+            double average;
+            Console.WriteLine("Hello, what is your full name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your grade?");
+            grade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your student ID?");
+            ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your login?");
+            login = Console.ReadLine();
+            Console.WriteLine("What is your average?");
+            double.TryParse(Console.ReadLine(), out average);
+            Console.WriteLine("Nice! " + average + "% Is pretty good");
+        }
+
+
+        public static void Part1()
+        {
             string name;
             int age;
             double money;
@@ -13,23 +42,22 @@
             Console.WriteLine("Nice to meet you, " + name + ", How old are you?");
             age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("So you're " + age + " eh? Thats not old at all! How much do you make " + name + "?");
-            money = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(); + money + "! I hope thats per hour and not per year");
-            
-            
-            
-            
-            
-            
+            double.TryParse(Console.ReadLine(), out money);
+            Console.WriteLine(money.ToString("C") + "! I hope thats per hour and not per year");
+        }
 
 
 
 
-
-
-
-
-
+        public static void Part3()
+        {
+            string name;
+            int age;
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hey " + name + " How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Did you know that in five years you will be " + (age + 5) + " years old? And five years ago you were " + (age - 5) + "! Imagine that!");
         }
     }
 }
